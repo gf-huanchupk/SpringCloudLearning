09647497500cf15fe00b0b4eed9625f9df38c69b
huanchu <782969359@qq.com>
2018-09-28 01:38:00 +0800

    第六篇 : 分布式配置中心（Spring Cloud Config）

---
+++ chapter06/config-client/src/main/java/com/gf/ConfigClientApplication.java
@@ -0,0 +1,25 @@
+package com.gf;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+	@Value("${foo}")
+	String foo;
+
+	@GetMapping("/hi")
+	public String hi() {
+		return foo;
+	}
+
+}
