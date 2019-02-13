2c5c5ca1413b33ae81331a727c9b794ca1612890
huanchu <782969359@qq.com>
2019-02-13 17:30:26 +0800

    第十三篇 : 路由网关（Gateway）

---
+++ chapter13/springcloud-gateway-hello/src/main/java/com/gf/SpringcloudGatewayHelloApplication.java
@@ -0,0 +1,53 @@
+package com.gf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+@RestController
+public class SpringcloudGatewayHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( SpringcloudGatewayHelloApplication.class, args );
+    }
+
+    /**
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://httpbin.org:80"))
+                .build();
+    }*/
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p
+                        .host("*.hystrix.com")
+                        .filters(f -> f.hystrix(config -> config
+                                .setName("mycmd")
+                                .setFallbackUri("forward:/fallback")))
+                        .uri("http://httpbin.org:80"))
+                .build();
+    }
+
+    @RequestMapping("/fallback")
+    public String fallback() {
+        return "fallback";
+    }
+
+}
+
