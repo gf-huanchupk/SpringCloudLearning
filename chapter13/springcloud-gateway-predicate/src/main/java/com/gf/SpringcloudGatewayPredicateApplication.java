93149d5037b4af2e05c2cd7cbc2369458dfed806
huanchu <782969359@qq.com>
2019-02-15 16:20:15 +0800

    第十三篇 : 路由网关（Gateway）

---
+++ chapter13/springcloud-gateway-predicate/src/main/java/com/gf/SpringcloudGatewayPredicateApplication.java
@@ -0,0 +1,14 @@
+package com.gf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudGatewayPredicateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( SpringcloudGatewayPredicateApplication.class, args );
+    }
+
+}
+
